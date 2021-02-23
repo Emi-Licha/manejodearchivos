795c4d79485a5adcbbb4098964b481ff3091812b
Emi Licha <emilianolicha@gmail.com>
2021-02-23 10:55:54 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,62 @@
+import express from 'express'
+
+import fs from 'fs'
+const app = express()
+app.use(express.json())
+
+
+let productos:any[] = []
+
+
+
+
+app.get('/productos', (req, res) => {
+    if (productos == []){
+        res.json({'error': 'No hay productos cargados'})
+    }
+    res.json(productos)
+})
+
+app.post('/productos', (req, res) => {
+    let { nombre, precio} = req.body
+    const id = productos.length + 1;
+    let producto = {
+        
+        nombre,
+        precio,
+        id
+    }
+    
+    productos.push(producto)
+    let data = JSON.stringify(productos,null,2);
+    fs.writeFileSync('./productos.txt', data, 'utf-8')
+    
+    res.send(producto)
+})
+app.delete('/productos', (req,res)=>{
+    const path = './productos.txt'
+
+    fs.unlink(path, (err) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+   
+    })
+        res.send('el archivo ha sido borrado')
+    
+})
+app.get('/productos/:id', (req,res) =>{
+    const id = req.params.id
+    const producto = productos.find(producto => producto.id === id)
+    if (!producto){
+        res.send('Producto no encontrado')
+    }
+    res.json(producto)
+})
+
+
+app.listen(8080, () =>{
+    console.log("Running on port 8080")
+})
